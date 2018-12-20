1f04b6eb6f2cd7c6c7660354b9e487db24b565fd
kadariyaujwal <cooloozewall@gmail.com>
2018-12-20 11:05:33 +0545

    slug binding to route and show question route created

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -57,6 +57,8 @@ class QuestionController extends Controller
     public function show(Question $question)
     {
         //
+        $question->increment('views');
+        return view('questions.show')->withQuestion($question);
     }
 
     /**
